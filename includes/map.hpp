9a76d1c60812096c0addb019717f4e6b8b7a3ac9
Ullhor Greyiron <ugreyiro@er-a6.kzn.21-school.ru>
2021-06-30 16:21:28 +0300

    map ready to debug

--- includes/map.hpp
+++ includes/map.hpp
@@ -35,14 +35,30 @@ public:
 		return (*this);
 	}
 
-	reference operator*() const	{ return _p->data; }
+	reference operator*() const	{ return *(_p->data); }
 
-	pointer operator->() const	{ return &(_p->data);}
+	pointer operator->() const	{ return (_p->data);}
 
 
 	MapIterator& operator--()
 	{
-		//TODO:this
+		if (_p->left)
+		{
+			_p = _p->left;
+			while (_p->right)
+				_p = _p->right;
+		}
+		else
+		{
+			nodeType* prev;
+			do
+			{
+				prev = _p;
+				_p = _p->parent;
+			}
+			while (_p && _p->left == prev);
+		}
+		return *this;
 	}
 
 	MapIterator operator--(int)
@@ -54,7 +70,23 @@ public:
 
 	MapIterator & operator++()		//prefix++
 	{
-		//TODO:this
+		if (_p->right)
+		{
+			_p = _p->right;
+			while (_p->left)
+				_p = _p->left;
+		}
+		else
+		{
+			nodeType* prev;
+			do
+			{
+				prev = _p;
+				_p = _p->parent;
+			}
+			while (_p && _p->right == prev);
+		}
+		return *this;
 	}
 
 	MapIterator operator++(int)	//postfix ++
@@ -82,13 +114,112 @@ public:
 
 template <class T, class nodeType>
 class MapConstIterator : public std::iterator<std::bidirectional_iterator_tag, T> {
+private:
+	const nodeType*	_p;
 
+public:
 	typedef nodeType	node_type;
 	typedef T			value_type;
 	typedef value_type&	reference;
 	typedef value_type*	pointer;
 
+	MapConstIterator() :_p(0)	{}
 
+	MapConstIterator(const MapConstIterator & other) : _p(other._p) {}
+
+	MapConstIterator(const MapIterator<T, node_type> & other)	{ _p = other.get_p(); }
+
+	MapConstIterator(node_type *p) : _p(p) {}
+
+	virtual ~MapConstIterator()	{}
+
+	MapConstIterator & operator=(const MapConstIterator & rh)
+	{
+		if (this != &rh)
+			this->_p = rh._p;
+		return (*this);
+	}
+
+	MapConstIterator & operator=(const MapIterator<T, node_type> & rh)
+	{
+		this->_p = rh.get_p();
+			return (*this);
+	}
+
+	reference operator*() const	{ return *(_p->data); }
+
+	pointer operator->() const	{ return _p->data;}
+
+	MapConstIterator& operator--()
+	{
+		if (_p->left)
+		{
+			_p = _p->left;
+			while (_p->right)
+				_p = _p->right;
+		}
+		else
+		{
+			const nodeType* prev;
+			do
+			{
+				prev = _p;
+				_p = _p->parent;
+			}
+			while (_p && _p->left == prev);
+		}
+		return *this;
+	}
+
+	MapConstIterator operator--(int)
+	{
+		MapConstIterator tmp(*this);
+		--(*this);
+		return (tmp);
+	}
+
+	MapConstIterator & operator++()		//prefix++
+	{
+		if (_p->right)
+		{
+			_p = _p->right;
+			while (_p->left)
+				_p = _p->left;
+		}
+		else
+		{
+			const nodeType* prev;
+			do
+			{
+				prev = _p;
+				_p = _p->parent;
+			}
+			while (_p && _p->right == prev);
+		}
+		return *this;
+	}
+
+	MapConstIterator operator++(int)	//postfix ++
+	{
+		MapConstIterator tmp(*this);
+		++(*this);
+		return (tmp);
+	}
+
+	friend bool operator==(const MapConstIterator & lh, const MapConstIterator & rh)
+	{
+		return (lh._p == rh._p);
+	}
+
+	friend bool operator!=(const MapConstIterator & lh, const MapConstIterator & rh)
+	{
+		return (lh._p != rh._p);
+	}
+
+	node_type* get_p() const
+	{
+		return (_p);
+	}
 };
 
 template <class Key, class T,
@@ -98,7 +229,7 @@ class map {
 
 private:
 	typedef struct s_node {
-		ft::pair<const Key, T>*		data;
+		ft::pair<const Key, T>		*data;
 		struct s_node				*parent;
 		struct s_node				*left;
 		struct s_node				*right;
@@ -140,52 +271,332 @@ public:
 		 typename ft::enable_if<std::__is_input_iterator<InputIt>::value>::type* = 0)
 	{
 		initMap(comp, alloc);
-		//TODO: add inserts
+		insert(first, last);
 	}
 
 	map( const map& other )
 	{
-		//TODO: this
+		initMap();
+		insert(other.begin(), other.end());
 	}
 
-	map( const map& other, const Allocator& alloc )
+	~map()
 	{
-		//TODO: this
+		//TODO: clear
+		_nodeAlloc.destroy(_nil);
+		_nodeAlloc.deallocate(_nil, 1);
 	}
 
-	ft::pair<iterator, bool> insert(const value_type &value )
+	map& operator=( const map& other )
 	{
-		if (_root == 0)
+		if (*this != other)
 		{
-			_root = new_node((value_type &) value);
-			_root->parent = 0;
-			return (ft::make_pair(iterator(_root), true));
+			map<Key, T, Compare, Allocator> tmp = map(other);
+			swap(*this, tmp);
 		}
-		else
-		{
-			_node* place = findPlace(value, _root);
-			if (!(value._first < place->data->_first) && !(value._first > place->data->_first))
-				return ft::make_pair(iterator(place), false);
+		return *this;
+	}
 
-			_node* newNode = new_node(value);
-			newNode->parent = place;
-			if (value._first < place->data->_first)
-				place->left = newNode;
-			else if (value._first > place->data->_first)
-				place->right = newNode;
-			return ft::make_pair(iterator(newNode), true);
+	T& operator[]( const Key& key )
+	{
+		return insert(ft::make_pair(key, mapped_type()))._first->_second;
+	}
+
+
+	/*****ITERATORS*****/
+
+	/**
+	 * returns most left node
+	 * @return mapIterator
+	 */
+	iterator begin()
+	{
+		_node* node = _root;
+		while (node->left)
+			node = node->left;
+		return (iterator(node));
+	}
+
+	const_iterator begin() const
+	{
+		_node* node = _root;
+		while (node->left)
+			node = node->left;
+		return (const_iterator(node));
+	}
+
+	/**
+	 * returns nil
+	 */
+	iterator end() { return iterator(_nil); }
+
+	const_iterator end() const { return const_iterator(_nil); }
+
+	/*****CAPACITY*****/
+	bool empty() const { return _nil == _root; }
+
+	size_type size() const
+	{
+		size_type size = 0;
+		for(iterator it = begin(); it != end(); it++)
+			++size;
+		return size;
+	}
+
+	size_type max_size() const { return std::numeric_limits<size_type>::max() / sizeof(value_type); }
+
+	/*****MODIFIERS*****/
+	void clear() { erase(begin(), end()); }
+
+	ft::pair<iterator, bool> insert(const value_type &value)
+	{
+		if (_root == _nil)
+		{
+			_node* node = new_node(value, _root->parent);
+			node->right = _nil;
+			_nil->parent = node;
+			_root = node;
+			return ft::make_pair(iterator(node), true);
 		}
+
+		_node* place = findPlace(value._first, _root);
+		if (_comp(value._first, place->data->_first))
+		{
+			_node* node = new_node(value, place);
+			place->left = node;
+			return ft::make_pair(iterator(node), true);
+		}
+		else if (_comp(place->data->_first, value._first))
+		{
+			_node* node = new_node(value, place);
+
+			if (place->right == _nil)
+			{
+				node->right = _nil;
+				_nil->parent = node;
+			}
+
+			place->right = node;
+			return ft::make_pair(iterator(node), true);
+		}
+		return ft::make_pair(iterator(place), false);
 	}
 
 	iterator insert( iterator hint, const value_type& value )
 	{
-		//TODO: this
+		_node* local_root = hint.get_p();
+		if (local_root == _nil)
+		{
+			_node* node = new_node(value, _root->parent);
+			node->right = _nil;
+			_nil->parent = node;
+			_root = node;
+			return iterator(node);
+		}
+
+		_node* place = findPlace(value._first, local_root);
+		if (_comp(value._first, place->data->_first))
+		{
+			_node* node = new_node(value, place);
+			place->left = node;
+			return ft::make_pair(iterator(node), true);
+		}
+		else if (_comp(place->data->_first, value._first))
+		{
+			_node* node = new_node(value, place);
+
+			if (place->right == _nil)
+			{
+				node->right = _nil;
+				_nil->parent = node;
+			}
+
+			place->right = node;
+			return iterator(node);
+		}
+		return iterator(place);
 	}
 
 	template< class InputIt >
-	void insert( InputIt first, InputIt last )
+	void insert( InputIt first, InputIt last,
+				 typename ft::enable_if<std::__is_input_iterator<InputIt>::value>::type* = 0)
 	{
-		//TODO: this
+		while (first != last)
+		{
+			insert(*first);
+			++first;
+		}
+	}
+
+
+	void erase( iterator pos )
+	{
+		_node* node = pos.get_p();
+		_nodeAlloc.destroy(node);
+		_nodeAlloc.deallocate(node, 1);
+	}
+
+	void erase( iterator first, iterator last )
+	{
+		for (; first != last; first++)
+			erase(first);
+	}
+
+	void swap( map& other )
+	{
+		allocator_type tmp_alloc = _alloc;
+		this->_alloc = other._alloc;
+		other._alloc = tmp_alloc;
+
+		nodeAllocator tmp_nodeAlloc = _nodeAlloc;
+		this->_nodeAlloc = other._nodeAlloc;
+		other._nodeAlloc = tmp_nodeAlloc;
+
+		_node*	tmp_root = _root;
+		this->_root = other._root;
+		other._root = tmp_root;
+
+		_node*	tmp_nil = _nil;
+		this->_nil = other._nil;
+		other._nil = tmp_nil;
+
+		Compare tmp_compare = _comp;
+		this->_comp = other._comp;
+		other._comp = tmp_compare;
+	}
+
+	/*****LOOKUP*****/
+	size_type count( const Key& key ) const
+	{
+		if(findEqual(key, _root))
+			return 1;
+		else
+			return 0;
+	}
+
+	iterator find( const Key& key )
+	{
+		_node* place = findEqual(key, _root);
+		if (place)
+			return iterator(place);
+		else
+			return iterator(_nil);
+	}
+
+	const_iterator find( const Key& key ) const
+	{
+		_node* place = findEqual(key, _root);
+		if (place)
+			return const_iterator(place);
+		else
+			return const_iterator(_nil);
+	}
+
+	ft::pair<iterator,iterator> equal_range( const Key& key )
+	{
+		iterator it1 = lower_bound(key);
+		iterator it2(it1);
+		if (it1 != end())
+			++it2;
+		return ft::make_pair(it1, it2);
+	}
+
+	ft::pair<const_iterator,const_iterator> equal_range( const Key& key ) const
+	{
+		return ft::make_pair(lower_bound(key), upper_bound(key));
+	}
+
+	iterator lower_bound( const Key& key )
+	{
+		iterator it1 = begin();
+		while (it1 != end())
+		{
+			if (!_comp(key, it1->_first))
+				break;
+		}
+		return it1;
+	}
+
+	const_iterator lower_bound( const Key& key ) const
+	{
+		const_iterator it1 = begin();
+		while (it1 != end())
+		{
+			if (!_comp(key, it1->_first))
+				break;
+		}
+		return it1;
+	}
+
+	iterator upper_bound( const Key& key )
+	{
+		iterator it1 = begin();
+		while (it1 != end())
+		{
+			if (_comp(key, it1->_first))
+				break;
+		}
+		return it1;
+	}
+
+	const_iterator upper_bound( const Key& key ) const
+	{
+		const_iterator it1 = begin();
+		while (it1 != end())
+		{
+			if (_comp(key, it1->_first))
+				break;
+		}
+		return it1;
+	}
+
+	key_compare key_comp() const
+	{
+		return _comp;
+	}
+
+	value_compare value_comp() const
+	{
+		return value_compare();
+	}
+
+	/*****NON-MEMBER******/
+	friend bool operator==( const ft::map<Key,T,Compare,Allocator>& lhs,
+					 const ft::map<Key,T,Compare,Allocator>& rhs )
+	{
+		if (lhs.size() != rhs.size() || lhs._comp != rhs._comp || lhs._alloc != rhs._alloc)
+			return false;
+		return equal(lhs.begin(), lhs.end(), rhs.begin());
+	}
+
+	friend bool operator!=( const ft::map<Key,T,Compare,Allocator>& lhs,
+					 const ft::map<Key,T,Compare,Allocator>& rhs )
+	{
+		return !(lhs == rhs);
+	}
+
+	friend bool operator<( const ft::map<Key,T,Compare,Allocator>& lhs,
+						   const ft::map<Key,T,Compare,Allocator>& rhs )
+	{
+		return (lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()));
+	}
+
+	friend bool operator>( const ft::map<Key,T,Compare,Allocator>& lhs,
+						   const ft::map<Key,T,Compare,Allocator>& rhs )
+	{
+		return (!(lhs < rhs) && !(lhs == rhs));
+	}
+
+	friend bool operator<=( const ft::map<Key,T,Compare,Allocator>& lhs,
+					 const ft::map<Key,T,Compare,Allocator>& rhs )
+	{
+		return (!(lhs > rhs));
+	}
+
+	friend bool operator>=( const ft::map<Key,T,Compare,Allocator>& lhs,
+					const ft::map<Key,T,Compare,Allocator>& rhs )
+	{
+		return (!(lhs < rhs));
 	}
 
 private:
@@ -195,10 +606,10 @@ typedef typename allocator_type::template rebind<_node>::other nodeAllocator;
 allocator_type	_alloc;
 nodeAllocator	_nodeAlloc;
 _node*			_root;
+_node*			_nil;
 Compare			_comp;
-size_type		_sz;
 
-_node* new_node(const value_type& pair)
+_node* new_node(const value_type& pair, _node* parent)
 {
 	_node* newNode = _nodeAlloc.allocate(1);
 	_nodeAlloc.construct(newNode);
@@ -206,6 +617,7 @@ _node* new_node(const value_type& pair)
 	_alloc.construct(newNode->data, pair);
 	newNode->left = 0;
 	newNode->right = 0;
+	newNode->parent = parent;
 	return (newNode);
 }
 
@@ -213,28 +625,43 @@ void initMap(const Compare comp, const Allocator& alloc)
 {
 	_comp = comp;
 	_alloc = alloc;
-	_sz = 0;
-	_root = 0;
+
+	_root = _nodeAlloc.allocate(1);
+	_nodeAlloc.construct(_root);
+	_root->left = 0;
+	_root->right = 0;
+	_root->parent = 0;
+	_nil = _root;
 }
 
-_node*	findPlace(const value_type& value, _node* root)
+_node*	findPlace(const Key key, _node* root)
 {
-	if(value._first < root->data->_first)
+
+	if(_comp(key, root->data->_first))
 	{
 		if (root->left == 0)
 			return root;
 		else
-			return findPlace(value, root->left);
+			return findPlace(key, root->left);
 	}
-	else if (value._first > root->data->_first)
+	else if (_comp(root->data->_first, key))
 	{
-		if (root->right == 0)
+		if (root->right == 0 || root->right == _nil)
 			return root;
 		else
-			return findPlace(value, root->right);
+			return findPlace(key, root->right);
 	}
 	else
 		return root;
+}
+
+_node*	findEqual(const Key key, _node* root)
+{
+	_node* place = findPlace(key, _root);
+	if (!_comp(place->data->_first, key) && !_comp(key, place->data->_first))
+		return place;
+	else
+		return 0;
 }
 
 };
